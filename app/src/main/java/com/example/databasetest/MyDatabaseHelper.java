5cb5a305eed7e344bd26088053a9f3a78ef981e2
Mikejiawei <15270013718@163.com>
2019-07-17 21:07:36 +0800

    update my database

--- app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -12,6 +12,11 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             + "author text, "
             + "price real, "
             + "name text)";
+    public static final String CREATE_CATEGORY = "create table Category("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)" ;
+
     private Context mContext;
 
     public MyDatabaseHelper(Context context,String name,SQLiteDatabase.CursorFactory factory,int version){
@@ -22,10 +27,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext,"Created succeeded",Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
